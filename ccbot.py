ebfee7a877c56c21f4a1674eaedf1e2044de90c6
John S Carignani <john@carignani.co.uk>
2020-09-24 14:20:36 +0100

    Total rewrite. v0.6 is functional but requires a strict format for the cc command.

--- ccbot.py
+++ ccbot.py
@@ -14,58 +14,101 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot as BotBase
 
-version = '0.0.1'
+version = '0.6'
 bot_token = os.getenv('CCBOT_TOKEN')
-#bot_token = os.environ['CCBOT_TOKEN'] # try this if it fails on Windows
 
 
 ###############################################################################
-# Bot Class
+# Bot Initialisation
 ###############################################################################
 
-class Bot(BotBase):
-    def __init__(self):
-        self.ready = False
+bot = BotBase(
+    command_prefix = commands.when_mentioned,
+    description = 'I change role colours for minimum wage.',
+    owner_ids = 0,
+    case_insensitive = True,
+    )
+
+@bot.event
+async def on_connect():
+    print('CCBot has connected to Discord...')
 
 
-        super().__init__(
-            command_prefix = commands.when_mentioned,
-            description = 'I change role colours for minimum wage.',
-            owner_ids = 0,
-            case_insensitive = True,
-            )
+@bot.event
+async def on_disconnect():
+    print('CCBot has disconnected from Discord...')
 
-        def run(self, version):
-            self.VERSION = version
-            super().run(bot_token, reconnect=True)
 
-        async def on_connect(self):
-            print('CCBot has connected to Discord...')
+@bot.event
+async def on_ready():
+    print('CCBot is ONLINE.')
 
-        async def on_disconnect(self):
-            print('CCBot has disconnected from Discord...')
 
-        async def on_ready(self):
-            if not self.ready:
-                self.ready = True
-                print('CCBot is ONLINE.')
-            else:
-                print('CCBot reconnected.')
+###############################################################################
+# Bot Commands
+###############################################################################
+
+@bot.command()
+async def about(ctx):
+    """
+    Show the "about" message
+    """
+    embed_about = discord.Embed(
+        title = 'About ccbot',
+        description = 'A simple bot for changing role colours.',
+        color = 0x00ccff
+        )
+
+    embed_about.add_field(
+        name = 'Author',
+        value = 'John C',
+        inline = False
+        )
+    embed_about.add_field(
+        name = 'Source Code',
+        value = 'https://www.github.com/lvl-6/',
+        inline = False
+        )
+
+    await ctx.send(embed=embed_about)
+
+
+@bot.command()
+@commands.has_guild_permissions(manage_roles=True)
+async def cc(ctx, colour: str):
+    """
+    Change the colour of your role
+    Pass colour as a hexadecimal RGB value beginning with 0x
+    i.e. for red 0xFF0000
+    """
+    desired_colour = int(colour, 0)
+
+    # Get the highest role of the user
+    user_role_list = ctx.message.author.roles
+    list_length = len(user_role_list)
+    highest_role = user_role_list[list_length-1:]
+    highest_role_name = ''
+    for role in highest_role:
+        highest_role_name = role.name
+
+    # Edit the colour of user's highest role
+    for role in ctx.guild.roles:
+        if role.name == highest_role_name:
+            await role.edit( color = desired_colour )
 
 
 ###############################################################################
 # Python functions
 ###############################################################################
 
-
-
+# Nothing here
 
 ###############################################################################
 # Program Execution
 ###############################################################################
 
-# Define bot and run
-bot = Bot()
+# Execution
 bot.run(bot_token)
 
 # Termination
+print('CCBot has been terminated.')
